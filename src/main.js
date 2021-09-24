c05261e294ed0ccda1aaf9a74ab8a89e74974b7c
Oscar Dao <43139435+oscardao@users.noreply.github.com>
2021-09-24 02:08:41 +0200

    Add styling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 
+const loader = new THREE.LoadingManager();
+
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xfffefc);
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -10,12 +13,9 @@ onResize();
 window.addEventListener("resize", onResize);
 window.addEventListener("orientationchange", onResize);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+camera.position.z = 8;
+camera.position.y = 7;
+camera.rotation.x = -0.7;
 
 function update() {
     requestAnimationFrame(update);
@@ -25,5 +25,14 @@ update();
 
 function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+setupRoom();
+
+function setupRoom() {
+    loader.load('3DModels/island.fbx', function (gltf) {
+        scene.add(gltf.scene);
+    });
 }
